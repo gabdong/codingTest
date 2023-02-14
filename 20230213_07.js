be6f14380c6efaba7a3b9509d5c83da611c2560a
gabdong <hwan970104@gmail.com>
2023-02-15 00:18:29 +0900

    no messagae

--- 20230213_07.js
+++ 20230213_07.js
@@ -1,28 +1,28 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/64061
 
 function solution(board, moves) {
-    var answer = 0;
-    const basket = [];
+  var answer = 0;
+  const basket = [];
 
-    for (let position of moves) {
-        position = position - 1;
+  for (let position of moves) {
+    position = position - 1;
 
-        for (const boardLine of board) {
-            const item = boardLine[position];
+    for (const boardLine of board) {
+      const item = boardLine[position];
 
-            if (item === 0) continue;
+      if (item === 0) continue;
 
-            if (basket[basket.length - 1] === item) {
-                basket.pop();
-                answer = answer + 2;
-            } else {
-                basket.push(item);
-            }
-            boardLine[position] = 0;
+      if (basket[basket.length - 1] === item) {
+        basket.pop();
+        answer = answer + 2;
+      } else {
+        basket.push(item);
+      }
+      boardLine[position] = 0;
 
-            break;
-        }
+      break;
     }
+  }
 
-    return answer;
+  return answer;
 }
