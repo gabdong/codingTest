0f0bd16dfe97a0c244e54d144ea99e7dd68f8dd8
gabdong <hwan970104@gmail.com>
2023-02-21 18:27:47 +0900

    2023.02.21

---
+++ 20230221_03.js
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17679
+
+function solution(m, n, board) {
+    var answer = 0;
+    return answer;
+}
+
+solution(6, 6, [
+    "TTTANT", 
+    "RRFACC", 
+    "RRRFCC", 
+    "TRRRAA", 
+    "TTMMMF", 
+    "TMMTTJ"
+]);
