7adf6be34686908fa12542e90ba1890132168c70
gabdong <hwan970104@gmail.com>
2023-02-24 18:48:31 +0900

    2023.02.24

---
+++ 20230224_01.js
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/72411
+
+function solution(orders, course) {
+    var answer = [];
+
+    return answer;
+}
+
+// solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]);
+solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]);
